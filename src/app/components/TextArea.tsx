e34e199a5943651fa22e365655e34f57d4259e67
Edwin Cosme <adriancosme15@gmail.com>
2023-05-19 15:52:41 -0500

    feat: add text area component

---
+++ src/app/components/TextArea.tsx
@@ -0,0 +1,47 @@
+import { InputHTMLAttributes, TextareaHTMLAttributes } from "react";
+import { FieldErrors, UseFormRegister, UseFormTrigger } from "react-hook-form";
+import { InputError } from "../styled-components/input-error.styled.component";
+const formValidation = (errors: FieldErrors, errorKey: string) => {
+  return errors[errorKey] ? (
+    <InputError className="error-message">
+      {errors[errorKey]?.message?.toString()}
+    </InputError>
+  ) : (
+    ""
+  );
+};
+interface InputProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  register: UseFormRegister<any>;
+  name: string;
+  errors?: FieldErrors;
+  disabled?: boolean;
+  trigger?: UseFormTrigger<any>;
+  placeholder?: string;
+}
+
+export default function TextArea({
+  register,
+  name,  
+  disabled,
+  trigger,
+  placeholder,
+  errors,
+  ...remaining
+}: InputProps) {
+  return (
+    <div>
+      <textarea
+        id={name}
+        disabled={disabled}
+        placeholder={placeholder}
+        {...register(name)}
+        onChange={() => {
+          trigger && trigger();
+        }}
+        {...remaining}
+        className={`w-full font-[600] outline-none m-0 shadow-none px-4 py-2 rounded-md ${errors[name] ? 'border-2 border-red-500' : ''}`}
+      />
+      {errors && formValidation(errors, name)}
+    </div>
+  );
+}
