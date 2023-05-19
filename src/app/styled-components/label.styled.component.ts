dd9aba90ff04e27bf8ba25a8e87feef79c6a89c7
Edwin Cosme <adriancosme15@gmail.com>
2023-05-19 15:52:19 -0500

    add label styled component

---
+++ src/app/styled-components/label.styled.component.ts
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const Label = styled.label`
+  font-size: 0.8rem;
+  font-weight: 600;
+  text-transform: uppercase;
+  display: flex;
+  flex-direction: column;
+  color: #565656;
+  line-height: 15px;
+  justify-content: space-between;  
+`
