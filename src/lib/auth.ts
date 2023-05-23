d72ea1f0e992e992da9c88ce06ce7261d9570bc0
Edwin Cosme <adriancosme15@gmail.com>
2023-05-23 18:50:56 -0500

    feat: add provider infojobs-next-auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,82 +1,17 @@
+import InfojobsProvider from "infojobs-next-auth-provider";
 import { NextAuthOptions } from "next-auth";
-
 export const authOptions: NextAuthOptions = {
     pages: {
         signIn: '/',
         signOut: '/',
     },
     providers: [
-        {
-            id: 'infojobs',
-            name: 'Infojobs',
-            type: 'oauth',
-            version: '2.0',
-            clientId: process.env.INFOJOBS_CLIENT_ID,
-            clientSecret: process.env.INFOJOBS_CLIENT_SECRET,
-            checks: ['none'],
-            authorization: {
-                url: process.env.INFOJOBS_AUTH_URL,
-                params: {
-                    scope: process.env.INFOJOBS_SCOPES,
-                    redirect_uri: process.env.INFOJOBS_CALLBACK_URL,
-                    response_type: "code",
-                },
-            },
-            token: {
-                url: process.env.INFOJOBS_AUTHORIZATION_URL,
-                async request({ params }) {
-                    const tokenUrl = new URL(process.env.INFOJOBS_AUTHORIZATION_URL ?? '');
-                    tokenUrl.searchParams.append('grant_type', 'authorization_code');
-                    tokenUrl.searchParams.append('code', params.code ?? '');
-                    tokenUrl.searchParams.append('redirect_uri', process.env.INFOJOBS_CALLBACK_URL ?? '');
-                    tokenUrl.searchParams.append('client_id', process.env.INFOJOBS_CLIENT_ID ?? '');
-                    tokenUrl.searchParams.append('client_secret', process.env.INFOJOBS_CLIENT_SECRET ?? '');
-                    const response = await fetch(tokenUrl.toString(), {
-                        method: 'POST',
-                    })
-                    const tokens = await response.json();
-                    return {
-                        tokens
-                    }
-                }
-            },
-            userinfo: {
-                url: process.env.INFOJOBS_USERINFO_URL,
-                async request({ tokens }) {
-                    const basicToken = `Basic ${Buffer.from(`${process.env.INFOJOBS_CLIENT_ID}:${process.env.INFOJOBS_CLIENT_SECRET}`).toString('base64')}`;
-                    const bearerToken = `Bearer ${tokens.access_token}`;
-                    const response = await fetch(process.env.INFOJOBS_USERINFO_URL ?? '', {
-                        headers: {
-                            Authorization: `${basicToken},${bearerToken}`,
-                        },
-                    });
-                    const profile = await response.json();
-                    return {
-                        id: profile.id,
-                        email: profile.email,
-                        image: profile.photo,
-                        name: profile.name,
-                        sub: profile.id,
-                    }
-                }
-            },
-            profile(profile) {
-                return {
-                    id: profile.id,
-                    name: profile.name,
-                    email: profile.email,
-                    image: profile.photo,
-                }
-            },
-            style: {
-                logo: '/infojobs.svg',
-                logoDark: '/infojobs.svg',
-                bg: '#2088c2',
-                text: "#fff",
-                bgDark: "#7289DA",
-                textDark: "#fff",
-            }
-        }
+        InfojobsProvider({
+            clientId: process.env.INFOJOBS_CLIENT_ID ?? "",
+            clientSecret: process.env.INFOJOBS_CLIENT_SECRET ?? "",
+            infojobs_scopes: process.env.INFOJOBS_SCOPES ?? "",
+            redirect_uri: process.env.INFOJOBS_SCOPES ?? "",
+        }),
     ],
     callbacks: {
         async jwt({ token, account }) {
