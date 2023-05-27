4710fafd91c5771db4dcd69b23dc2835ced526da
Edwin Cosme <adriancosme15@gmail.com>
2023-05-27 08:34:19 -0500

    style: flex col on mobile

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -99,7 +99,7 @@ export default function ResumePage() {
 
   return (
     <div className="container-wrapper mt-4 w-full m-auto">
-      <div className="flex flex-row gap-4  w-full h-full">
+      <div className="flex flex-col gap-4  w-full h-full md:flex-row">
         <section className="flex-row items-start relative shadow-sm bg-white rounded-lg p-6 m-4 w-[320px]">
           <h1 className="pb-4 border-gray-300 block text-lg border-b font-bold">
             Tu experiencia
